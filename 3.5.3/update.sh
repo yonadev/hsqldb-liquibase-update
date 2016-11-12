ba17de0863693379f82605bfff0cb53a7b522111
Bert-R <bert@famroos.nu>
2016-11-12 08:43:41 +0100

    first commit

---
+++ 3.5.3/update.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+ERROR_EXIT_CODE=1
+
+echo "Setting up liquibase"
+: ${USER?"USER not set"}
+: ${PASSWORD?"PASSWORD not set"}
+: ${URL?"URL not set"}
+[ -f /changelogs/changelog.* ] || (echo "Cannot find a single change log matching /changelogs/changelog.*" ; exit $ERROR_EXIT_CODE)
+
+cat <<CONF > /home/rundir/liquibase.properties
+  driver: org.hsqldb.jdbc.JDBCDriver
+  classpath:/opt/jdbc_drivers/$DRIVER_JAR
+  url: $URL
+  username: $USER
+  password: $PASSWORD
+CONF
+
+echo "Applying changelogs ..."
+MAX_TRIES=${MAX_TRIES:-1}
+COUNT=1
+while [  $COUNT -le $MAX_TRIES ]; do
+   echo  "Attempting to apply changelogs: attempt $COUNT of $MAX_TRIES"
+   liquibase --changeLogFile="/changelogs/changelog.*" update
+   if [ $? -eq 0 ];then
+   	  echo "Changelogs successfully applied"
+      exit 0
+   fi
+   echo "Failed to apply changelogs"
+   sleep 1
+   let COUNT=COUNT+1
+done
+echo "Too many non-successful attempts"
+exit $ERROR_EXIT_CODE
---
+++ README.md
@@ -0,0 +1,24 @@
+Base layer for Liquibase update
+==================================
+
+This image is created as base layer. To use it, derive an image from it and add the root change log file to as /changelogs/changelog with your preferred file extension.
+
+This image runs [Liquibase update](http://www.liquibase.org/documentation/update.html) with the change log /changelogs/changelog.\* and the HSQLDB JDBC driver.
+
+#### Example use
+
+```
+docker run -i \
+  -e USER=sa \
+  -e PASSWORD=TopSecret \
+  -e URL=url \
+  your/image
+```
+
+The environment variables required are:
+
+USER - the user to the target database  
+PASSWORD - password to the target database  
+URL - the JDBC URL
+
+The implementation is based on [the work of Tom Beresford](https://hub.docker.com/r/beresfordt/pg-liquibase-update/~/dockerfile/).
