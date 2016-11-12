b3f17c5b857238f8eda722caaff43f57a42d8d12
Bert-R <bert@famroos.nu>
2016-11-12 12:37:30 +0100

    Correction in script
    HSQLDB driver downgraded to 2.3.3.

--- 3.5.3/update.sh
+++ 3.5.3/update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ERROR_EXIT_CODE=1
 
@@ -6,9 +6,10 @@ echo "Setting up liquibase"
 : ${USER?"USER not set"}
 : ${PASSWORD?"PASSWORD not set"}
 : ${URL?"URL not set"}
-[ -f /changelogs/changelog.* ] || (echo "Cannot find a single change log matching /changelogs/changelog.*" ; exit $ERROR_EXIT_CODE)
+CHANGE_LOG=`echo /changelogs/changelog.*`
+[ -f $CHANGE_LOG ] || (echo "Cannot find a single change log matching /changelogs/changelog.*" ; exit $ERROR_EXIT_CODE)
 
-cat <<CONF > /home/rundir/liquibase.properties
+cat <<CONF > liquibase.properties
   driver: org.hsqldb.jdbc.JDBCDriver
   classpath:/opt/jdbc_drivers/$DRIVER_JAR
   url: $URL
@@ -21,7 +22,7 @@ MAX_TRIES=${MAX_TRIES:-1}
 COUNT=1
 while [  $COUNT -le $MAX_TRIES ]; do
    echo  "Attempting to apply changelogs: attempt $COUNT of $MAX_TRIES"
-   liquibase --changeLogFile="/changelogs/changelog.*" update
+   liquibase --logLevel=info --changeLogFile=$CHANGE_LOG update
    if [ $? -eq 0 ];then
    	  echo "Changelogs successfully applied"
       exit 0
@@ -30,5 +31,5 @@ while [  $COUNT -le $MAX_TRIES ]; do
    sleep 1
    let COUNT=COUNT+1
 done
-echo "Too many non-successful attempts"
+echo "Too many failed attempts"
 exit $ERROR_EXIT_CODE
